7ef451de3555dc9fbc7f61311d378f36d2b88107
CrazyAaronM <hxm520WHL>
2022-04-19 11:07:46 +0800

    first commit

---
+++ rq-springboot-with/src/main/java/cn/enjoyedu/SpringbootWithApplication.java
@@ -0,0 +1,16 @@
+package cn.enjoyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+
+ *类说明：
+ */
+@SpringBootApplication
+public class SpringbootWithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithApplication.class, args);
+    }
+}
